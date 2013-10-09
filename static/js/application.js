ea6c0676d8638874c7387ddaadffee8801534a98
Masayoshi Sekimura <sekimura@gmail.com>
2013-10-09 00:08:21 -0700

    re-implement application.js with angular

--- static/js/application.js
+++ static/js/application.js
@@ -1,29 +1,51 @@
-var inbox = new ReconnectingWebSocket("ws://"+ location.host + "/receive");
-var outbox = new ReconnectingWebSocket("ws://"+ location.host + "/submit");
+angular.module("myApp", []);
 
-inbox.onmessage = function(message) {
-  var data = JSON.parse(message.data);
-  $("#chat-text").append("<div class='panel panel-default'><div class='panel-heading'>" + $('<span/>').text(data.handle).html() + "</div><div class='panel-body'>" + $('<span/>').text(data.text).html() + "</div></div>");
-  $("#chat-text").stop().animate({
-    scrollTop: $('#chat-text')[0].scrollHeight
-  }, 800);
-};
+// socket factory to have a singleton on both controllers
+angular.module("myApp").factory('socket', function ($location, $log) {
+  // communicate with Redis based pubsub server
+  var host = $location.host() + ":" + $location.port();
+  var inbox = new ReconnectingWebSocket("ws://" + host + "/receive");
+  var outbox = new ReconnectingWebSocket("ws://" + host + "/submit");
 
-inbox.onclose = function(){
-    console.log('inbox closed');
+  inbox.onclose = function () {
+    $log.info('inbox closed');
     this.inbox = new WebSocket(inbox.url);
+  };
 
-};
-
-outbox.onclose = function(){
-    console.log('outbox closed');
+  outbox.onclose = function () {
+    $log.info('outbox closed');
     this.outbox = new WebSocket(outbox.url);
-};
+  };
 
-$("#input-form").on("submit", function(event) {
-  event.preventDefault();
-  var handle = $("#input-handle")[0].value;
-  var text   = $("#input-text")[0].value;
-  outbox.send(JSON.stringify({ handle: handle, text: text }));
-  $("#input-text")[0].value = "";
+  return {
+    send: function (data) {
+      outbox.send(JSON.stringify(data));
+    },
+    onMessage: function (callback) {
+      inbox.onmessage = callback;
+    }
+  };
+});
+
+// Controller to display messages
+angular.module("myApp").controller('ChatMessagesCtrl', function ($scope, $timeout, socket) {
+  $scope.messages = [];
+  socket.onMessage(function (evt) {
+    $timeout(function () {
+      $scope.messages.push(JSON.parse(evt.data));
+    });
+  });
+});
+
+// Controller to send messages
+angular.module("myApp").controller('InputFormCtrl', function ($scope, $timeout, socket) {
+  this.submit = function () {
+    socket.send({
+      handle: $scope.handle,
+      text: $scope.text
+    });
+    $timeout(function () {
+      $scope.text = '';
+    });
+  };
 });
